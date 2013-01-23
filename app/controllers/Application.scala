82b3c217e3e9fe4c30f856f752a628ab2021c058
Flemming Hoffmeyer <flemming@schantz.com>
2013-01-23 19:31:14 +0100

    using person model in view and controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,15 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models.Person
 
 object Application extends Controller {
+
+val personForm = Form( "name" -> nonEmptyText )
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(Person.all(), personForm ))
   }
-  
 }
