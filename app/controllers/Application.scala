4bf59383f777f9425850e1788b392ad9535b126d
Flemming Hoffmeyer <flemming@schantz.com>
2013-01-23 19:42:23 +0100

    corrected some errors

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,13 +4,16 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import models.Person
+import models.Persons
 
 object Application extends Controller {
 
 val personForm = Form( "name" -> nonEmptyText )
   
   def index = Action {
-    Ok(views.html.index(Person.all(), personForm ))
+    Ok(views.html.index(Persons.all, personForm ))
+  }
+  def newPerson() = Action { implicit request =>
+    Ok("sdf")	
   }
 }
