965685800aac314a0e1da91cee21cbc184fc4a30
Flemming Hoffmeyer <flemming@schantz.com>
2013-04-16 10:12:05 +0200

    added config for mongoHQ

--- app/models/order.scala
+++ app/models/order.scala
@@ -1,10 +1,14 @@
 package models
+
+import play.api.Play.current
 import com.mongodb.casbah.Imports._
 import com.novus.salat._
 import com.novus.salat.global._
 import mongoContext._
 import com.novus.salat.dao.{ SalatDAO, ModelCompanion }
 import com.novus.salat.annotations._
+import se.radley.plugin.salat._
+import se.radley.plugin.salat.Binders._
 
 case class Order(
   @Key("_id") id: ObjectId = new ObjectId,
@@ -15,8 +19,7 @@ case class Order(
 )
 
 object Orders extends ModelCompanion[Order, ObjectId] {
-  val collection = MongoConnection()("food")("Orders")
-  val dao = new SalatDAO[Order, ObjectId](collection = collection) {}
-
+  def collection = mongoCollection("Orders")
+  val dao = new SalatDAO[Order, ObjectId](collection) {}
   def findByDinner(dinnerId: ObjectId) = dao.find(MongoDBObject("dinnerId" -> dinnerId)).toList
 } 
